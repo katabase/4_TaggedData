2a7c4645c6110af038c4a25398e3b363acc7c0d3
Alexandre Bartz <alex.bartz@outlook.fr>
2020-12-09 11:08:06 +0100

    convert str to float

--- script/extractor_json.py
+++ script/extractor_json.py
@@ -4,6 +4,17 @@ from lxml import etree
 
 ns = {'tei': 'http://www.tei-c.org/ns/1.0'}
 
+def to_float(str):
+	"""
+	This function tries to convert a string into a float.
+	:param str: a string
+	:return: a float or None
+	"""
+	try:
+		return float(str)
+	except:
+		return None
+
 
 def data_extractor(tree, output_dict):
 	"""
@@ -12,21 +23,21 @@ def data_extractor(tree, output_dict):
 	"""
 
 	# For each desc, a dict retrieve all the data.
-	for desc in tree.xpath('//tei:text//tei:item//tei:desc', namespaces=ns):
+	for desc in tree.xpath('.//tei:text//tei:item//tei:desc', namespaces=ns):
 		data = {}
 		desc_id = desc.xpath('./@xml:id', namespaces=ns)[0]
 		if desc.xpath('parent::tei:item/tei:measure[@quantity]', namespaces=ns):
-			data["price"] = desc.xpath('parent::tei:item//tei:measure[@commodity="currency"]/@quantity', namespaces=ns)[0]
+			data["price"] = to_float(desc.xpath('parent::tei:item//tei:measure[@commodity="currency"]/@quantity', namespaces=ns)[0])
 		else:
 			data["price"] = None
 		if desc.xpath('parent::tei:item/tei:name[@type="author"]/text()', namespaces=ns):
 			author = desc.xpath('parent::tei:item/tei:name[@type="author"]/text()', namespaces=ns)[0]
 			try:
-            # We keep only the surname of the author.
-            	author = author.split(" ")[0]
-            except:
-            	author = None
-            data["author"] = author
+            # We only keep the surname of the author.
+				author = author.split(" ")[0]
+			except:
+				author = None
+			data["author"] = author
 		else:
 			data["author"] = None
 		if desc.xpath('./tei:date[@when]', namespaces=ns):
@@ -34,11 +45,11 @@ def data_extractor(tree, output_dict):
 		else:
 			data["date"] = None
 		if desc.xpath('./tei:measure[@type="length"]', namespaces=ns):
-			data["number_of_pages"] = int(desc.xpath('./tei:measure[@type="length"]/@n', namespaces=ns)[0])
+			data["number_of_pages"] = to_float(desc.xpath('./tei:measure[@type="length"]/@n', namespaces=ns)[0])
 		else:
 			data["number_of_pages"] = None
 		if desc.xpath('./tei:measure[@type="format"]', namespaces=ns):
-			data["format"] = int(desc.xpath('./tei:measure[@type="format"]/@ana', namespaces=ns)[0])
+			data["format"] = desc.xpath('./tei:measure[@type="format"]/@ana', namespaces=ns)[0]
 		else:
 			data["format"] = None
 		if desc.xpath('./tei:term', namespaces=ns):
