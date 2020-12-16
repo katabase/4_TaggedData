eda1a0f80d4046ed001ef943a6ef2e9a7084b7e4
Alexandre Bartz <alex.bartz@outlook.fr>
2020-12-16 23:07:42 +0100

    change data type

--- script/test.py
+++ script/test.py
@@ -47,17 +47,17 @@ class Data_extraction(unittest.TestCase):
                 "author": "Barry",
                 "date": "1846",
                 "desc": "L. a. s. au colonel Fox; 1846, 1 p. in-8.",
-                "format": "#document_format_8",
+                "format": 8,
                 "number_of_pages": 1.0,
                 "price": 15.0,
                 "sell_date": "1887-11",
-                "term": "#document_type_7",
+                "term": 7,
             }
         }
 
         self.assertDictEqual(output_dict, test_dict)
 
-    def multiple_descs(self):
+    def test_multiple_descs(self):
         tree = etree.fromstring(
             """
   <TEI xmlns="http://www.tei-c.org/ns/1.0" xml:id="CAT_000112">
@@ -83,7 +83,7 @@ class Data_extraction(unittest.TestCase):
       <p>célèbre architecte anglais, qui construisit le Parlement de Westminster, né en
            1796, mort en 1860</p>
      </trait>
-     <desc xml:id="CAT_000112_e18_d1"><term ana="#document_type_7">L. a. s.</term> au colonel Fox; <date when="1846">1846</date>, <measure type="length" unit="p" n="1">1 p.</measure> <measure type="format" unit="f" ana="#document_format_8">in-8.</measure></desc><measure commodity="currency" unit="FRF" quantity="15">15</measure>
+     <desc xml:id="CAT_000112_e18_d1"><term ana="#document_type_7">L. a. s.</term> au colonel Fox; <date when="1846">1846</date>, <measure type="length" unit="p" n="1">1 p.</measure> <measure type="format" unit="f" ana="#document_format_8">in-8.</measure></desc>
      <desc xml:id="CAT_000112_e18_d2"><term ana="#document_type_7">L. a. s.</term> au colonel Fox; <date when="1846-08-01">1er août 1846</date>, <measure type="length" unit="p" n="3">3 p.</measure> <measure type="format" unit="f" ana="#document_format_4">in-4.</measure></desc><measure commodity="currency" unit="FRF" quantity="55">55</measure>
     </item>
    </text>
@@ -99,27 +99,27 @@ class Data_extraction(unittest.TestCase):
                 "author": "Barry",
                 "date": "1846",
                 "desc": "L. a. s. au colonel Fox; 1846, 1 p. in-8.",
-                "format": "#document_format_8",
+                "format": 8,
                 "number_of_pages": 1.0,
-                "price": 15.0,
+                "price": 55.0,
                 "sell_date": "1887-11",
-                "term": "#document_type_7",
+                "term": 7,
             },
             "CAT_000112_e18_d2": {
                 "author": "Barry",
                 "date": "1846-08-01",
                 "desc": "L. a. s. au colonel Fox; 1er août 1846, 3 p. in-4.",
-                "format": "#document_format_4",
+                "format": 4,
                 "number_of_pages": 3.0,
                 "price": 55.0,
                 "sell_date": "1887-11",
-                "term": "#document_type_7",
+                "term": 7,
             }
         }
 
         self.assertDictEqual(output_dict, test_dict)
 
-    def missing_data(self):
+    def test_missing_data(self):
         tree = etree.fromstring("""
   <TEI xmlns="http://www.tei-c.org/ns/1.0" xml:id="CAT_000112">
    <teiHeader xmlns:s="http://purl.oclc.org/dsdl/schematron" xmlns:tei="http://www.tei-c.org/ns/1.0">
@@ -158,11 +158,11 @@ class Data_extraction(unittest.TestCase):
                 "author": None,
                 "date": "1846",
                 "desc": "L. a. s. au colonel Fox; 1846, in-8.",
-                "format": "#document_format_8",
+                "format": 8,
                 "number_of_pages": None,
                 "price": 15.0,
                 "sell_date": "1887-11",
-                "term": "#document_type_7",
+                "term": 7,
             }
         }
 
